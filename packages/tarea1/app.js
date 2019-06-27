3a3c96e28f6c0461a3da196958316d5946d3f2cc
Krbaio3 <krbaio3@gmail.com>
2019-06-27 11:01:50 +0200

    monorepo

---
+++ packages/tarea1/app.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    name: 'KrBaIO',
+    age: 30,
+    link: 'https://picsum.photos/200'
+  },
+  methods: {
+    random: function() {
+      return Math.random();
+    }
+  }
+});
